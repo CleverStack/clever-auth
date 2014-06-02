63e8c3ff12a8d36060d59169b66f25ce75653a19
Richard Gustin <richard.gustin86@gmail.com>
2014-06-02 23:12:10 +1000

    Fixig prompt to add user to seed data

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,85 @@
 'use strict';
 
 var fs      = require( 'fs' )
-,   path    = require( 'path' )
+  , path    = require( 'path' )
+  , crypto  = require( 'crypto' );
 
 module.exports = function( grunt ) {
     return [{
-        exec: {
-            rebase: {
-                cmd: "NODE_PATH=./lib/:./modules/; node modules/clever-orm/bin/rebase.js"
-            },
-            seed: {
-                cmd: "NODE_PATH=./lib/:./modules/; node modules/clever-orm/bin/seedModels.js"
+        prompt: {
+            authSeedData: {
+                options: {
+                    questions: [
+                        {
+                            config: 'cleverauth.username',
+                            type: 'input',
+                            message: 'Admin Username',
+                            default: 'admin',
+                        },
+                        {
+                            config: 'cleverauth.password',
+                            type: 'password',
+                            message: 'Admin Users Password',
+                            default: 'password'
+                        },
+                        {
+                            config: 'cleverauth.email',
+                            type: 'input',
+                            message: 'Admin Users Email',
+                            default: 'default@email.com'
+                        },
+                        {
+                            config: 'cleverauth.firstname',
+                            type: 'input',
+                            message: 'Admin Users Firstname',
+                            default: 'Admin',
+                        },
+                        {
+                            config: 'cleverauth.lastname',
+                            type: 'input',
+                            message: 'Admin Users Lastname',
+                            default: 'User',
+                        },
+                        {
+                            config: 'cleverauth.phone',
+                            type: 'input',
+                            message: 'Admin Users Phone Number',
+                            default: ''
+                        }
+                    ]
+                }
             }
         }
     }, function( grunt ) {
-        // Register each command
-        grunt.registerTask( 'db:rebase', [ 'exec:rebase' ] );
-        grunt.registerTask( 'db:seed', [ 'exec:seed' ] );
+        grunt.loadNpmTasks('grunt-prompt');
 
-        // Register db command (runs one after the other)
-        grunt.registerTask( 'db', [ 'db:rebase', 'db:seed' ] );
+        grunt.registerTask( 'prompt:createAuthSeedData', [ 'prompt:authSeedData', 'authSeedData' ] );
+        grunt.registerTask( 'prompt:cleverCreateAuthSeedData', [ 'prompt:createAuthSeedData' ] );
+        grunt.registerTask( 'authSeedData', 'Creates seed data for clever-auth module', function ( ) {
+            var conf = grunt.config( 'cleverauth' )
+              , obj  = {
+                    'UserModel': []
+                }
+              , file = path.join( process.cwd(), 'modules', 'clever-auth', 'schema', 'seedData.json' );
+
+            if (fs.existsSync( file )) {
+                obj = require( file );
+            }
+
+            conf.active = true;
+            conf.confirmed = true;
+            conf.hasAdminRight = true;
+            conf.password = crypto.createHash( 'sha1' ).update( conf.password ).digest( 'hex' );
+
+
+            if ( obj[ 'UserModel' ][ 0 ] && obj[ 'UserModel' ][ 0 ].username === 'test' ) {
+                obj[ 'UserModel' ].shift();
+            }
+
+            obj[ 'UserModel' ] = obj[ 'UserModel' ] || [];
+            obj[ 'UserModel' ].push( conf );
+
+            fs.writeFileSync( file, JSON.stringify( obj, null, '  ' ) );
+        } );
     }];
 };

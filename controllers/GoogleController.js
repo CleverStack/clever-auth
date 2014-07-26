14443006f621fb944f1e6c6c3dc9ec794a5812e1
Richard Gustin <richard.gustin86@gmail.com>
2014-07-26 18:19:23 +1000

    feat(google): Refactored the google authentication system

---
+++ controllers/GoogleController.js
@@ -0,0 +1,52 @@
+var GoogleStrategy  = require( 'passport-google' ).Strategy
+  , injector        = require( 'injector' );
+
+module.exports = function( cleverAuth, Controller, passport, UserService, AuthController ) {
+    var googleEnabled = !!cleverAuth.config.enabledStrategies && !!cleverAuth.config.enabledStrategies.google
+      , googleConfig = !!googleEnabled ? cleverAuth.config.enabledStrategies.google : false;
+
+    return Controller.extend({
+
+        restfulRouting: false,
+
+        route: '/google',
+
+        autoRouting: googleEnabled,
+
+        setup: function() {
+            if ( !!googleEnabled ) {
+                injector.instance( 'GoogleStrategy', GoogleStrategy );
+
+                passport.use(
+                    new GoogleStrategy({
+                        returnURL:  googleConfig.returnUrl,  
+                        realm:      googleConfig.realm
+                    }, this.callback( 'googleLogin' ) )
+                );
+            }
+
+            return this._super.apply( this, arguments );
+        },
+
+        googleLogin: function( identifier, user, done ) {
+            UserService
+                .findOrCreate({
+                    email:              user.emails[ 0 ].value,
+                    firstName:          user.name.givenName,
+                    lastName:           user.name.familyName,
+                    googleIdentifier:   identifier
+                })
+                .then( done )
+                .catch( done.bind( null ) );
+        }
+    },
+    {
+        signInAction: function () {
+            passport.authenticate( 'google' )( this.req, this.res, this.next );
+        },
+
+        returnAction: function () {
+            passport.authenticate( 'google', this.proxy( AuthController.authenticate, null ) )( this.req, this.res, this.next );
+        }
+    });
+};
